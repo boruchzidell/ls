09d496c07886fa5285a2076e95880f700e186161
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2021-05-12 16:40:45 -0400

    caesar_cipher.js solution

--- 215/caesar_cipher.js
+++ 215/caesar_cipher.js
@@ -1,1 +1,28 @@
 #! /usr/bin/env node
+
+function caesarEncrypt(string, shift) {
+  return string.replace(/[a-z]/gi, (letter) => cipher(letter, shift));
+}
+
+function cipher(letter, shift) {
+  let offset = letter.toUpperCase() === letter ? 65 : 97;
+
+  let ascii = ((letter.codePointAt(0) - offset + shift) % 26) + offset;
+
+  return String.fromCodePoint(ascii);
+}
+
+// simple shift
+console.log(caesarEncrypt('A', 0) === "A");
+console.log(caesarEncrypt('A', 3) === "D");
+
+// wrap around
+console.log(caesarEncrypt('y', 5) === "d");
+console.log(caesarEncrypt('a', 47) === "v");
+
+// all letters
+console.log(caesarEncrypt('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 25) === "ZABCDEFGHIJKLMNOPQRSTUVWXY");
+console.log(caesarEncrypt('The quick brown fox jumps over the lazy dog!', 5) === "Ymj vznhp gwtbs ktc ozrux tajw ymj qfed itl!");
+
+// many non-letters
+console.log(caesarEncrypt('There are, as you can see, many punctuations. Right?; Wrong?', 2) === "Vjgtg ctg, cu aqw ecp ugg, ocpa rwpevwcvkqpu. Tkijv?; Ytqpi?");
